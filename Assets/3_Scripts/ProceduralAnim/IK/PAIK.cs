c73e4cf79f1173a87ad817ba2f35ceefa950d312
Robert Klares <klaro115@hotmail.com>
2018-09-12 23:19:51 +0200

    Started adding procedural animation system.

---
+++ Assets/3_Scripts/ProceduralAnim/IK/PAIK.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralAnim
+{
+	public abstract class PAIKGroup : MonoBehaviour
+	{
+		#region Fields
+
+		//...
+
+		#endregion
+		#region Methods
+
+		public abstract void initialize();
+
+		public abstract void update(float deltaTime);
+		
+		#endregion
+	}
+}
